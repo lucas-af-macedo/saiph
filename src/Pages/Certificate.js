b3e30e72064f5aac386eb487b7d3e25cbdcb476c
Lucas Macedo <lucasamacedo@hotmail.com>
2023-04-17 17:46:45 -0300

    feat: page certificate

--- src/Pages/Certificate.js
+++ src/Pages/Certificate.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 import React from "react";
+import styled from "styled-components";
 
 export default function Certificate() {
 	const [selectedFile, setSelectedFile] = React.useState(null);
@@ -35,10 +36,58 @@ export default function Certificate() {
 		});
 	}
 	return (
-		<form onSubmit={teste}>
+		<Container>
+			<Title>Cadastro do Certificado</Title>
+		<Form onSubmit={teste}>
 			<input type="file" name="file" onChange={handleFileUpload} />
-			<input type="text" name="text" onChange={handleFileUpload2} />
-			<button>teste</button>
-		</form>
+			<input type="text" name="text" placeholder="Senha" onChange={handleFileUpload2} />
+			<button>Enviar Certificado</button>
+		</Form>
+		</Container>
 	);
 }
+
+const Container = styled.div`
+    height: auto;
+    width: 100%;
+    margin-left: 50px;
+	margin-right: 50px;
+    margin-bottom: 50px;
+	padding-top: 30px;
+`
+
+const Title = styled.div`
+    font-size: 25px;
+    font-weight: 500;
+    margin-bottom: 40px;
+`
+
+const Form = styled.form`
+	display: flex;
+	flex-direction: column;
+	input{
+		max-width: 300px;
+		margin-bottom: 8px;
+		height: 30px;
+	}
+	button{
+		margin-top: 5px;
+		max-width: 300px;
+		height: 30px;
+		box-shadow: 0px 0px 2px black;
+		border-radius: 3px;
+		outline: 0px;
+		border: 0px;
+		background-color: #6082f0;
+		font-size: 15px;
+		color: white;
+		font-weight: 600;
+		transition: 400ms;
+		cursor: pointer;
+		:hover{
+			background-color: #f7f7ff;
+			color: #6082f0;
+		}
+	}
+		
+`
